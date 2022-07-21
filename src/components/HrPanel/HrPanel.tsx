d4c47ec8bcb3656709ccbdbc2d651c7cd4550d1c
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-21 11:17:49 +0200

    HrPanel in progress

---
+++ src/components/HrPanel/HrPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function HrPanel() {
+  return (
+    <div className="HrPanel">
+        <div className="HrPanel__select">
+            
+        </div>
+    </div>
+  )
+}
