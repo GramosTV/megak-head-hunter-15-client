7af62b944d3a2d4e1af5de0e14f0d2533d90f817
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-22 11:24:37 +0200

    Work in progress

--- src/components/HrPanel/HrPanel.tsx
+++ src/components/HrPanel/HrPanel.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AvailableStudents } from './AvailableStudents/AvailableStudents'
+import { StudentList } from './AvailableStudents/Student'
 import { Select } from './Select'
 
+// any because waiting for student types
 export function HrPanel() {
+  const [students, setStudents] = useState<any[]>([
+    {
+      name: 'Jan',
+      lastName: 'Kowalski',
+      email: '1234@test.pl',
+    },
+    {
+      name: 'Jan',
+      lastName: 'Kowalski',
+      email: '1235@test.pl',
+    },
+    {
+      name: 'Jan',
+      lastName: 'Kowalski',
+      email: '1236@test.pl',
+    },
+  ])
   return (
     <div className="hrPanel">
         <Select />
         <AvailableStudents />
+        <StudentList students={students}/>
     </div>
   )
 }
