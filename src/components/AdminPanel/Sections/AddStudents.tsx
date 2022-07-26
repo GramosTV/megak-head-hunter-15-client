4c12e0e5eab3474e0f1fb977ff9f53e5fbf20b03
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-26 19:47:14 +0200

    Began working on csv to json

--- src/components/AdminPanel/Sections/AddStudents.tsx
+++ src/components/AdminPanel/Sections/AddStudents.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Papa from 'papaparse';
 
 export function AddStudents() {
   return (
-    <div><button>Dodaj kursantów</button></div>
+    <div>
+      <input
+        type="file"
+        accept=".csv,.xlsx,.xls"
+        onChange={(e) => {
+          const files = e.target.files;
+          console.log(files);
+          if (files) {
+            console.log(files[0]);
+            Papa.parse(files[0], {
+              complete: function(results) {
+                console.log("Finished:", results.data);
+              }}
+            )
+          }
+        }}
+      />
+      <button>Dodaj kursantów</button></div>
   )
 }
