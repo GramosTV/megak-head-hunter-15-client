2ba699d41f695e00270b404a87fb919df2934667
Rafal <rgrajcar@gmail.com>
2022-07-27 17:55:25 +0200

    added AuthContextObj interface to Auth interfaces file

--- src/types/interfaces/Auth.ts
+++ src/types/interfaces/Auth.ts
@@ -1,4 +1,12 @@
+import { AuthUser } from "types";
+
 export interface LoginData {
   login: string;
   password: string;
+}
+
+export interface AuthContextObj {
+  user: AuthUser | null,
+  signIn: ({login, password}: LoginData) => void,
+  signOut: () => void,
 }
