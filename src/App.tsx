7baaa23f1bbb047240faf56545dfa554489c2014
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-28 12:03:29 +0200

    fixed app imports

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { HrPanel } from './components/HrPanel/HrPanel';
 import { LoginForm } from './components/LoginForm/LoginForm';
 import {Route, Routes} from "react-router-dom";
 import {ProtectedRoute} from "./components/ProtectedRoute/ProtectedRoute";
-
 import './App.css';
 import './style/global.css';
 import './style/HrPanel/hrPanel.css';
@@ -14,8 +13,6 @@ import './style/HrPanel/AvailableStudents/studentList.css';
 import './style/HrPanel/AvailableStudents/filter.css';
 import './style/LoginForm/loginForm.css';
 import './style/HrPanel/AvailableStudents/cv.css';
-import { HrPanel } from './components/HrPanel/HrPanel';
-import { LoginForm } from './components/LoginForm/LoginForm';
 
 
 export const App = () => {
