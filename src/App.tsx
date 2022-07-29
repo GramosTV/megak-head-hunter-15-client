e5aa3c9b28278df1e432c16157fa0562cbc23a06
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-29 09:50:12 +0200

    removed duplicate imports

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ import './style/LoginForm/loginForm.css';
 import './style/AdminPanel/adminPanel.css';
 import './style/AdminPanel/Sections/addHrForm.css';
 import './style/AdminPanel/Sections/addStudents.css';
-import { HrPanel } from './components/HrPanel/HrPanel';
-import { LoginForm } from './components/LoginForm/LoginForm';
 import { AdminPanel } from './components/AdminPanel/AdminPanel';
 import './style/HrPanel/AvailableStudents/cv.css';
 
