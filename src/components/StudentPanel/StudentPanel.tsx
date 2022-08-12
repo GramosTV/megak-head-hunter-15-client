2d6d66fc02b6d95340bfcfe997774a908cf80094
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-08-12 17:15:31 +0200

    Student Profile Config

--- src/components/StudentPanel/StudentPanel.tsx
+++ src/components/StudentPanel/StudentPanel.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Select } from "./Select";
-import { User, GetPaginatedListOfUser, Status } from "types";
+import {
+  User,
+  GetPaginatedListOfUser,
+  Status,
+  UserProfile,
+  ExpectedTypeWork,
+  ExpectedContractType,
+  BoolValues,
+} from "types";
 import { UserFE } from "src/types/interfaces/UserFE";
 import { FilterSettings } from "types";
 import { toast } from "react-toastify";
@@ -13,11 +21,32 @@ export function StudentPanel() {
   const [panelSection, setPanelSection] = useState<StudentPanelEnum>(
     StudentPanelEnum.profile
   );
+  const [studentProfile, setStudentProfile] = useState<UserProfile>({
+    email: "",
+    tel: 0,
+    firstName: "",
+    lastName: "",
+    githubUsername: "",
+    portfolioUrls: ["ok"],
+    projectUrls: ["ok, ok"],
+    bio: "",
+    expectedTypeWork: ExpectedTypeWork.All,
+    targetWorkCity: "",
+    expectedContractType: ExpectedContractType.B2B,
+    expectedSalary: 0,
+    canTakeApprenticeship: BoolValues.FALSE,
+    monthsOfCommercialExp: 0,
+    education: "",
+    workExperience: "",
+    courses: "",
+  });
 
   return (
     <div className="studentPanel">
       <Select panelSection={panelSection} setPanelSection={setPanelSection} />
-      {panelSection === StudentPanelEnum.profile ? <Profile /> : null}
+      {panelSection === StudentPanelEnum.profile ? (
+        <Profile studentProfile={studentProfile} />
+      ) : null}
       {panelSection === StudentPanelEnum.changePassword ? (
         <ChangePassword />
       ) : null}
