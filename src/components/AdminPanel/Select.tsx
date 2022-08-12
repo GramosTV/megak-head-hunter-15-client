af948b3b4af8bd86d776cbadc4d4b9816580802b
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-08-12 10:20:43 +0200

    Renames

--- src/components/AdminPanel/Select.tsx
+++ src/components/AdminPanel/Select.tsx
@@ -1,20 +1,26 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { AdminSectionEnum } from "../../types/enums/adminSectionEnum";
+import { AdminSectionEnum } from "../../types/enums/AdminSectionEnum";
 
 interface SelectProps {
   adminSection: AdminSectionEnum;
   setAdminSection: Dispatch<SetStateAction<AdminSectionEnum>>;
 }
 
-export function Select({adminSection, setAdminSection}: SelectProps) {
+export function Select({ adminSection, setAdminSection }: SelectProps) {
   return (
-  <div className="adminPanel__select">
-    <div className={`hrPanel__option ${!adminSection ? "active" : ""}`} onClick={() => setAdminSection(AdminSectionEnum.addHr)}>
-    Dodaj HR
+    <div className="adminPanel__select">
+      <div
+        className={`adminPanel__option ${!adminSection ? "active" : ""}`}
+        onClick={() => setAdminSection(AdminSectionEnum.addHr)}
+      >
+        Dodaj HR
+      </div>
+      <div
+        className={`adminPanel__option ${adminSection ? "active" : ""}`}
+        onClick={() => setAdminSection(AdminSectionEnum.importStudents)}
+      >
+        Dodaj kursantów
+      </div>
     </div>
-    <div className={`adminPanel__option ${adminSection ? "active" : ""}`} onClick={() => setAdminSection(AdminSectionEnum.importStudents)}>
-    Dodaj kursantów
-    </div>
-  </div>
   );
 }
